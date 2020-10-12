de88770ee777c01c2a4d3489a0f0899054a6d4f9
shubhamkhaitan <65844445+shubhamkhaitan@users.noreply.github.com>
2020-10-12 16:06:33 +0530

    Update common.module.js

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
     "use strict";
     
     angular.module('common', [])
-    .constant('ApiPath', 'https://aleksandar-gjorgievski.herokuapp.com')
+    .constant('ApiPath', 'https://fathomless-thicket-75994.herokuapp.com')
     .config(config);
     
     config.$inject = ['$httpProvider'];
